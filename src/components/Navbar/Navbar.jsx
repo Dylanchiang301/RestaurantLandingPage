eba32ba08e6be2590b1e6aaf6407fb28db533542
MinYu <cmydylan@gmail.com>
2023-11-14 07:39:03 +0800

    2023111修改

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
@@ -10,8 +10,8 @@ import './Navbar.css';
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
-  return(
-    
+  return (
+
     <nav className='app__navbar'>
       {/* <nav><nav/>tag是用來製作導覽列的tag，會搭配<a><a/>加入連結。 */}
       <div className='app__navbar-logo'>
@@ -31,24 +31,24 @@ const Navbar = () => {
 
       <div className='app__navbar-login'>
         <a href='#login' className='p__opensans'>LOG IN / REGISTER</a>
-        <div/>
+        <div />
         <a href='#home' className='p__opensans'>BOOK TABLE</a>
       </div>
-      
-      <div className='app__navbar-smallscreen'>
-        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)}/>
 
-        {toggleMenu &&( 
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+
+        {toggleMenu && (
           // 這邊表示if toggleMenu的值是true，就會執行下方程式
           <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
             <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ui className="app__navbar-smallscreen_links">
-              <li className='p__opensans'><a href='#home'>HOME</a></li>
-              <li className='p__opensans'><a href='#about'>ABOUT</a></li>
-              <li className='p__opensans'><a href='#menu'>MENU</a></li>
-              <li className='p__opensans'><a href='#awards'>AWARDS</a></li>
-              <li className='p__opensans'><a href='#contact'>CONTACT</a></li>
-           </ui>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href='#home'>HOME</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href='#about'>ABOUT</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href='#menu'>MENU</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href='#awards'>AWARDS</a></li>
+              <li className='p__opensans' onClick={() => setToggleMenu(false)}><a href='#contact'>CONTACT</a></li>
+            </ui>
           </div>
         )}
       </div>
